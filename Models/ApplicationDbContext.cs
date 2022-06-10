b98508c306e94b52d87705be7ac1f9b4ddf76189
710Val <86447935+710Val@users.noreply.github.com>
2022-06-09 21:15:25 -0300

    Add files via upload

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Simbora.Models
+{
+     public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Sugestao> Sugestoes { get; set; }
+
+        public DbSet<Eventos> Eventos { get; set; }
+
+        public DbSet<Usuarios> Usuario { get; set; }
+        
+
+    } 
+}
+
+    
+      
+ 
+    
+
